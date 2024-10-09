de21a240d5b079ffd1f2c8cc5332707ec68dfa62
unknown <2314290084@ACADEMICO.IESB.BR>
2024-10-09 10:57:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CalculadoraIMC from './Components/imc' // Ajuste o caminho se necessário
+
+const App = () => {
+  return (
+    <div>
+      <CalculadoraIMC />
+    </div>
+  );
+};
+
+export default App;
+
